7e8c6ba91068f88d2bbde5738836b3d69ea0f7ad
sssooomin <jkshine7@gmail.com>
2022-04-01 18:51:21 +0900

    new

---
+++ test2.js
@@ -0,0 +1,11 @@
+        id = prompt('아이디를 입력해주세요.');
+        if(id=='egoing'){
+            password = prompt('비밀번호를 입력해주세요.');
+            if(password==='111111'){
+                alert('인증 했습니다.');
+            } else {
+                alert('인증에 실패 했습니다.');
+            }
+        } else {
+            alert('인증에 실패 했습니다.');
+        }
