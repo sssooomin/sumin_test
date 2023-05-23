4f0ff2f27dca4dfa822d1800081b434404fae4bc
sssooomin <jkshine7@gmail.com>
2023-05-23 12:53:24 +0900

    java-calc

---
+++ OP01.java
@@ -0,0 +1,42 @@
+package fire.java.src.op;
+
+public class OP01 {
+
+	public static void main(String[] args) {
+		// 산술 연산자
+		
+		int i1 = 5 + 5;
+		int i2 = 5 - 5;
+		int i3 = 5 * 5;
+		int i4 = 10 / 3;
+		int i5 = 10 % 3;
+		
+		
+		
+		//2. 대입 연산자
+		
+		// +=
+		//d1 += 10; // d1 = d1 + 10;
+		
+		//-=
+		int d2 = 200;
+		d2 -= 100;
+		System.out.println("d2 = " + d2);
+		//*=
+		
+		// %=
+		
+		
+		// 3. 증감 연산자
+		
+		// ++
+		
+		int j1 = 10;
+		System.out.println("j1 = " + j1);
+		++j1;
+		
+		
+		
+	}
+
+}
---
+++ OP02.java
@@ -0,0 +1,17 @@
+package fire.java.src.op;
+
+public class OP02 {
+
+	public static void main(String[] args) {
+		
+		// 4. 비교 연산자
+		int a1 = 10;
+		int a2 = 20;
+		
+		// > <  >= <= == !=
+		boolean flag1 = a1 != a2;
+		System.out.println("flag1 = " + flag1);
+
+	}
+
+}
